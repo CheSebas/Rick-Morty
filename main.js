03c14e6189c4748956090285cddb5b10f13b5457
Mario <marioc720@gmail.com>
2025-08-01 17:50:34 -0300

    menubtn responsivo faltan funcionalidad

--- main.js
+++ main.js
@@ -103,3 +103,47 @@ document.getElementById("toggleFilters").addEventListener("click", () => {
     const container = document.getElementById("filtersContainer");
     container.classList.toggle("hidden");
 });
+
+//---------------Menu Hambuerguesa-------------------------
+const menu = document.getElementById('mobileMenu');
+const btnMenuH = document.getElementById('svgMenuH');
+
+// Toggle para abrir/cerrar desde el botón
+btnMenuH.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menu.classList.toggle('hidden');
+});
+
+// Cierra el menú al hacer clic en cualquier enlace dentro de él
+document.querySelectorAll('#mobileMenu a').forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.add('hidden');
+    });
+});
+
+// Cierra el menú si se hace clic fuera de él
+document.addEventListener('click', (e) => {
+    const isClickInside = menu.contains(e.target) || btnMenuH.contains(e.target);
+    if (!isClickInside) {
+        menu.classList.add('hidden');
+    }
+});
+
+//----------------FIN MENU HAMBURGUESA-----------------------
+//agregando clases dinamicamente a menú en header
+const nav = document.getElementById('miNav');
+const enlaces = nav.children;
+
+for (let i = 0; i < enlaces.length; i++) {
+    enlaces[i].classList.add(
+        "text-white",
+        "text-2xl",
+        "lg:text-3xl",
+        "xl:text-5xl",
+        "hover:bg-gray-400",
+        "active:scale-95",
+        "transition-transform",
+        "p-2",
+        "rounded-2xl"
+    );
+};
